50a6f5ffbd354a2f740c472404f588aeb7dac314
harshit kumar <harshit.page@gmail.com>
2025-06-18 23:03:34 +0530

    -a

---
+++ src/Rendercard.jsx
@@ -0,0 +1,59 @@
+import Card from './card/Card';
+import pfp from './assets/pfp.jpeg';
+import anuj from './assets/anuj.jpeg';
+import vinayak from './assets/vinayak.png';
+import ayush from './assets/ayush.png';
+import anish from './assets/anish.png';
+
+function Rendercard()
+{
+    card_data = [
+        {
+            name: "harshit heya",
+            designation: "product designer",
+            image: {harshit},
+            portfolio: "https://bento.me/harshit-heya"
+        },
+        {
+            name: "being anuj",
+            designation: "investment banker",
+            image: {anuj},
+            portfolio: "https://bento.me/harshit-heya"
+        },
+        {
+            name: "harshit heya",
+            designation: "product designer",
+            image: {harshit},
+            portfolio: "https://bento.me/harshit-heya"
+        },
+        {
+            name: "harshit heya",
+            designation: "product designer",
+            image: {harshit},
+            portfolio: "https://bento.me/harshit-heya"
+        },
+        {
+            name: "harshit heya",
+            designation: "product designer",
+            image: {harshit},
+            portfolio: "https://bento.me/harshit-heya"
+        },
+        {
+            name: "harshit heya",
+            designation: "product designer",
+            image: {harshit},
+            portfolio: "https://bento.me/harshit-heya"
+        },
+        {
+            name: "harshit heya",
+            designation: "product designer",
+            image: {harshit},
+            portfolio: "https://bento.me/harshit-heya"
+        },
+    ]
+    return(
+        <card />
+    )
+}
+
+export default Rendercard
