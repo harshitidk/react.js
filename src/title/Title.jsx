f92e66b82bdd455a5fdbd4a837854974f503de46
harshit kumar <harshit.page@gmail.com>
2025-06-22 13:25:58 +0530

    -a

--- src/title/Title.jsx
+++ src/title/Title.jsx
@@ -4,7 +4,7 @@ function Title(){
     return(
         <div className='title'>
             <div className="title-heading">club of the (<span>certified</span>) menaces</div>
-            <div className="title-description"><span>this is the place where all the kings reside</span>, checkout their linkedin or portfolios, hire them, vibe with them, or just be impressed - they’re wild - both in the boardroom and in life..<span> this is a forbidden treasury of the kings</span>, only a few selected can make it to this point so enjoyyyy!!!.</div>
+            <div className="title-description"><span>this is the place where all the pookie bears reside</span>, checkout their linkedin or portfolios, hire them, connect with them, or just be impressed - this is a vault of the rare ones, the kind you don’t just stumble upon..<span> the forbidden treasury of the pookie bears</span>, only a few selected can make it to this point so enjoyyyy!!!.</div>
         </div>
     );
 }
